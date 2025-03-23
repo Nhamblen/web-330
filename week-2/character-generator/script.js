9845d5c12d701c042b93a7f14e2f8caa29175cb3
Noah <nhamblen21@gmail.com>
2025-03-23 11:25:39 -0500

    Add JavaScript to create character

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -11,15 +11,29 @@
 'use strict';
 
 function createCharacter(name, gender, characterClass) {
-  // TODO: Implement this function
+  return {
+    getName: () => name,
+    getGender: () => gender,
+    getClass: () => characterClass,
+  };
 }
 
 document.getElementById('generateHero').addEventListener('click', function (e) {
   e.preventDefault();
 
-  // TODO: Get form values
+  // Form values
+  const name = document.getElementById('heroName').value;
+  const gender = document.getElementById('heroGender').value;
+  const characterClass = document.getElementById('heroClass').value;
 
-  // TODO: Create character
+  // Create character
+  const character = createCharacter(name, gender, characterClass);
 
-  // TODO: Display character information
+  // Display character information
+  const output = document.getElementById('characterOutput');
+  output.innerHTML = `
+    <p><strong>Name:</strong> ${character.getName()}</p>
+    <p><strong>Gender:</strong> ${character.getGender()}</p>
+    <p><strong>Class:</strong> ${character.getClass()}</p>
+  `;
 });
