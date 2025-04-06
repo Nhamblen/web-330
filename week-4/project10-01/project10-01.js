4dfb912dec1c6e15e49d4b52ced58ebafcab4d39
Noah <nhamblen21@gmail.com>
2025-04-06 13:49:22 -0500

    Rename and add comments to begin project 10-1

--- week-4/project10-01/project10-01.js
+++ week-4/project10-01/project10-01.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 /*    JavaScript 7th Edition
       Chapter 10
       Project 10-01
 
       Project to create a drag and drop jigsaw puzzle
-      Author: 
-      Date:   
+      Author: Noah Hamblen
+      Date: 4/6/25
 
       Filename: project10-01.js
 */
 
 // Reference to the puzzle board
-let puzzleBoard = document.getElementById("puzzleBoard");
+let puzzleBoard = document.getElementById('puzzleBoard');
 // Counter for the zIndex style of each puzzle piece
 let zCounter = 1;
 // Array of integers from 1 to 48
@@ -21,25 +21,24 @@ let intList = new Array(48);
 let pointerX, pointerY, pieceX, pieceY;
 
 // Sort the integers from 1 to 48 in random order
-for (let i = 0; i < 48 ; i++) {
-   intList[i] = i+1;
+for (let i = 0; i < 48; i++) {
+  intList[i] = i + 1;
 }
-intList.sort(function() {
-   return 0.5 - Math.random();
+intList.sort(function () {
+  return 0.5 - Math.random();
 });
 
 // generate randomly-sorted puzzle pieces
 for (let i = 0; i < 48; i++) {
-   let piece = document.createElement("img");
-   piece.src = "piece" + intList[i] + ".png";
-   let rowNum = Math.ceil((i+1)/8);
-   let colNum = (i + 1) - (rowNum - 1)*8;
-   piece.style.top = (rowNum - 1)*98 + 7 + "px";
-   piece.style.left = (colNum - 1)*98 + 7 + "px";
-   piece.draggable = false; // override the default draggability of images
-   puzzleBoard.appendChild(piece);      
+  let piece = document.createElement('img');
+  piece.src = 'piece' + intList[i] + '.png';
+  let rowNum = Math.ceil((i + 1) / 8);
+  let colNum = i + 1 - (rowNum - 1) * 8;
+  piece.style.top = (rowNum - 1) * 98 + 7 + 'px';
+  piece.style.left = (colNum - 1) * 98 + 7 + 'px';
+  piece.draggable = false; // override the default draggability of images
+  puzzleBoard.appendChild(piece);
 }
 
 // Node list representing the puzzle pieces
-let pieces = document.querySelectorAll("div#puzzleBoard img");
-
+let pieces = document.querySelectorAll('div#puzzleBoard img');
