2a6b8759288e80b5328f41db55df1822c2f9dce2
Noah <nhamblen21@gmail.com>
2025-04-19 14:35:06 -0500

    Link and update js with fetch

--- week-6/project11-02/project11-02.js
+++ week-6/project11-02/project11-02.js
@@ -10,9 +10,39 @@
       Filename: project11-02.js
 */
 
+// Variables for the input fields
 let postalCode = document.getElementById('postalCode');
 let place = document.getElementById('place');
 let region = document.getElementById('region');
 let country = document.getElementById('country');
 
-postalCode.onblur = function () {};
+// Event listener for when the user moves focus away (on blur)
+postalCode.onblur = function () {
+  // Get the postal code and country values entered by the user
+  let codeValue = postalCode.value;
+  let countryValue = country.value;
+
+  // Clear any existing city and state values in the fields
+  place.value = '';
+  region.value = '';
+
+  // Fetch city and state data using the Zippopotam API
+  fetch(`http://api.zippopotam.us/${countryValue}/${codeValue}`)
+    .then((response) => {
+      // Check if the response is successful, otherwise throw an error
+      if (!response.ok) {
+        throw new Error('Invalid postal code');
+      }
+      // Parse the response data as JSON
+      return response.json();
+    })
+    .then((json) => {
+      // Fill the place and state abbreviation fields with data from the API response
+      place.value = json.places[0]['place name'];
+      region.value = json.places[0]['state abbreviation'];
+    })
+    .catch((error) => {
+      // Log any errors to the console
+      console.error('Error:', error.message);
+    });
+};
