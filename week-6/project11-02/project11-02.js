c4c1775cf0939bba020088932daafff1dc98dbe4
Noah <nhamblen21@gmail.com>
2025-04-19 13:43:05 -0500

    Rename and format files to begin project 11-2

---
+++ week-6/project11-02/project11-02.js
@@ -0,0 +1,18 @@
+'use strict';
+/*    JavaScript 7th Edition
+      Chapter 11
+      Project 11-02
+
+      Project to city and state information from a provided postal code
+      Author: Noah Hamblen
+      Date: 4/19/25
+
+      Filename: project11-02.js
+*/
+
+let postalCode = document.getElementById('postalCode');
+let place = document.getElementById('place');
+let region = document.getElementById('region');
+let country = document.getElementById('country');
+
+postalCode.onblur = function () {};
