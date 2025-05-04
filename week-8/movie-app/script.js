cce2ce2100e5cc09cf3c085ec643b277d0010851
Noah <nhamblen21@gmail.com>
2025-05-03 20:18:08 -0500

    Make array of movie objects

--- week-8/movie-app/script.js
+++ week-8/movie-app/script.js
@@ -10,8 +10,31 @@
 
 'use strict';
 
+// Array of movie objects
 const movies = [
-  // Add your movie objects here
+  {
+    title: 'Interstellar',
+    director: 'Christopher Nolan',
+    year: 2014,
+    synopsis:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+  },
+
+  {
+    title: 'The Joker',
+    director: 'Todd Phillips',
+    year: 2019,
+    synopsis:
+      'A troubled comedian named Arthur Fleck descends into madness and transforms into the criminal known as the Joker, sparking chaos and unrest across the city.',
+  },
+
+  {
+    title: 'IT',
+    director: 'Andrés Muschietti',
+    year: 2017,
+    synopsis:
+      'A group of kids in Derry confront a terrifying shape-shifting clown that feeds on their fears.',
+  },
 ];
 
 function fetchMovie(title) {
