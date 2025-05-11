2262308305a799e02c4b734cc62351d9301a42d8
Noah <nhamblen21@gmail.com>
2025-05-10 19:26:17 -0500

    Format files to begin week 9 assignment

--- week-9/project12-03/project12-03.js
+++ week-9/project12-03/project12-03.js
@@ -1,13 +1,11 @@
-"use strict";
+'use strict';
 /*    JavaScript 7th Edition
       Chapter 12
       Project 12-03
 
       Project to show a recipe with expanding/contracting content
-      Author: 
-      Date:   
+      Author: Noah Hamblen
+      Date: 5/10/25
 
       Filename: project12-03.js
 */
-
-
